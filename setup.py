535f0a529d8e42495f52e03d865f1ac42b740c7d
Sudharshan <imsudharshan281@gmail.com>
2024-09-27 11:26:22 +0530

    added readme

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='tuneparams',
-    version='0.1.4',
+    version='0.1.5',
     packages=find_packages(),
     install_requires=['astor', 'tqdm'],
     entry_points={
@@ -11,9 +11,9 @@ setup(
         ],
     },
     include_package_data=True,
-    author='Sudharshan|Ashish|Risha_Lab',
+    author='Sudharshan | Ashish | Risha Lab',
     author_email='imsudharshan281@gmail.com',
     description='A CLI tool to modify and execute Python scripts.',
     license='MIT',
-    url='https://github.com/sudharshan281/tuneX',
+    url='https://github.com/Sudharshan281/tuneparams',
 )
